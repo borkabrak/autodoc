b57622e27c3114fd9d82c267af5246e44928c3db
Jonathan Carter <jcarter@str.com>
2013-05-01 16:30:29 -0500

    Removed a bit of useless code

--- bin/didoc.js
+++ bin/didoc.js
@@ -7,14 +7,4 @@ $(function(){
             return false;
         }
     );
-
-
-    //periodic_reload();
-
 });
-
-var periodic_reload = function(){
-    // DEBUG: reload the page every few seconds.
-    return setTimeout(function(){window.location.reload()},3000);
-};
-
