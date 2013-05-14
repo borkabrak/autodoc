00e88584592b68cf1a4ea282416855e15c72053d
Jonathan Carter <jcarter@str.com>
2013-05-14 13:25:03 -0500

    Ajaxify <a> elements.

--- didoc.js
+++ didoc.js
@@ -1,10 +1,23 @@
-
 $(function(){
-    $("a.back").on(
+
+    var mkd = new Markdown.Converter();
+
+    // Ajax-y behaviour for links
+    $("a").on(
         'click',
-        function(){
-            window.history.back();
-            return false;
+        function(event){
+            console.log("Link %s clicked!", $(event.currentTarget).html());
+
+            $.get( 
+                event.currentTarget.href,
+
+                function(data, status, xhr){
+                    console.log("Success!");
+                    $(".content").html(mkd.makeHtml(data));
+                }
+            );
+
+            return false; // Elide normal link behaviour
         }
     );
 });
