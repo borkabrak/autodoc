f9cd6789290ec97c1987f3dc889d47715c177c92
Jonathan Carter <jcarter@str.com>
2013-05-14 16:03:07 -0500

    A little error handling for bad links.

--- didoc.js
+++ didoc.js
@@ -1,17 +1,25 @@
 $(function(){
 
     // Ajax-y behaviour for links
-    $("a").on(
+    $(document).on(
         'click',
+        'a',
         function(event){
 
-            $.get( 
-                event.currentTarget.href,
+            $.ajax({ 
 
-                function(data, status, xhr){
-                    $("#content").change_to(data);
-                }
-            );
+                url: 
+                    event.currentTarget.href,
+
+                success: 
+                    function(data, status, xhr){
+                    $("#content").change_to(data); },
+
+                error:
+                    function(xhr){
+                        console.log("XHR: %o",xhr);
+                        $("#content").change_to(errmsg(xhr)); }
+            });
 
             return false; // Elide normal link behaviour
         }
@@ -40,3 +48,14 @@ $.prototype.change_to = function(data){
         }
     );
 };
+
+errmsg = function(xhr){
+    console.log(xhr.getAllResponseHeaders());
+    switch (xhr.status){
+    case 404:
+        return $("<p><span class='error'>That file does not seem to exist</span>");
+        break;
+    default:
+        return $("<p><span class='error'>There was an error handling that click.  My fault.  Yell at me about it at jcarter@str.com</span>")
+    }
+}
