3e886ccb0675d8cf6976032d0b4e3dc074cc9f37
Jonathan Carter <jcarter@str.com>
2013-05-14 13:29:20 -0500

    Removed JS markdown converter

--- didoc.js
+++ didoc.js
@@ -1,19 +1,15 @@
 $(function(){
 
-    var mkd = new Markdown.Converter();
-
     // Ajax-y behaviour for links
     $("a").on(
         'click',
         function(event){
-            console.log("Link %s clicked!", $(event.currentTarget).html());
 
             $.get( 
                 event.currentTarget.href,
 
                 function(data, status, xhr){
-                    console.log("Success!");
-                    $(".content").html(mkd.makeHtml(data));
+                    $(".content").html(data);
                 }
             );
 
