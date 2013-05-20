42784458de789da0333f670053226b996b1ecbf4
Jonathan Carter <jcarter@str.com>
2013-05-20 11:43:45 -0500

    Seperate function for loading pages and allow off-site links to behave normally.

--- didoc.js
+++ didoc.js
@@ -1,36 +1,15 @@
 $(function(){
 
     // Ajax-y behaviour for links
-    $(document).on(
-        'click',
-        'a',
+    $(document).on( 'click',
+
+        'a:not([href^=http])',
+
         function(event){
-
-            // If the link goes off-site, don't interfere.
-            // FIXME: This is a really hacky way to do it..
-            if (event.currentTarget.href.match("ushen048w") === null){;
-                return true;
-            }
-
-            $.ajax({ 
-
-                url: 
-                    event.currentTarget.href,
-
-                success: 
-                    function(data, status, xhr){
-                    $("#content").change_to(data); },
-
-                error:
-                    function(xhr){
-                        console.log("XHR: %o",xhr);
-                        $("#content").change_to(errmsg(xhr)); }
-            });
-
+            navigate(event.currentTarget.pathname);
             return false; // Elide normal link behaviour
         }
     );
-
 
     // Load initial home page
     $.get(
@@ -56,7 +35,6 @@ $.prototype.change_to = function(data){
 };
 
 errmsg = function(xhr){
-    console.log(xhr.getAllResponseHeaders());
     switch (xhr.status){
     case 404:
         return $("<p><span class='error'>That file does not seem to exist</span>");
@@ -64,4 +42,21 @@ errmsg = function(xhr){
     default:
         return $("<p><span class='error'>There was an error handling that click.  My fault.  Yell at me about it at jcarter@str.com</span>")
     }
+}
+
+function navigate(pagename) {
+
+    $.ajax({ 
+
+        url: 
+            pagename,
+
+        success: 
+            function(data, status, xhr){
+            $("#content").change_to(data); },
+
+        error:
+            function(xhr){
+                $("#content").change_to(errmsg(xhr)); }
+    });
 }
