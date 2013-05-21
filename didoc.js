7bb2326e27d4fc27c3e0df635efe0d72dfed4493
Jonathan Carter <jcarter@str.com>
2013-05-21 11:31:06 -0500

    NavHistory object handles more on its own.

--- didoc.js
+++ didoc.js
@@ -1,6 +1,6 @@
 $(function(){
 
-    var nav = new NavHistory();
+    var nav = new NavHistory($("#content"));
 
     // Ajax-y behaviour for links
     $(document).on( 'click',
@@ -23,28 +23,6 @@ $(function(){
     // Load initial home page
     nav.newpage("/didoc/homepage.html");
 
-    $.prototype.change_to = function(data){
-        // Change content of an element in a groovy way.
-        var elem = this;
-
-        elem.fadeOut(
-            0,
-            function(){
-                elem.html(data);
-                elem.fadeIn();
-            }
-        );
-    };
-
-    var errmsg = function(xhr){
-        switch (xhr.status){
-        case 404:
-            return $("<p><span class='error'>That file does not seem to exist</span>");
-            break;
-        default:
-            return $("<p><span class='error'>There was an error handling that click.  My fault.  Yell at me about it at jcarter@str.com</span>")
-        }
-    }
 
 });
 
--- nav-history.js
+++ nav-history.js
@@ -1,35 +1,18 @@
-var NavHistory = function(){
+var NavHistory = function(elem){
     my = this;
-    my.history = [];   
 
-    // index into history representing current site
+    my.elem = $(elem);  // Content container
+
+    my.history = [];   
     my.current = 0;
 
-    // private
-    var go = function(url, success){
-
-        $.ajax({ url: url,
-
-            success: function(data, status, xhr){
-                    $("#content").change_to(data); 
-                    if (success){
-                        success.call();
-                    }
-            },
-
-            error: function(xhr){
-                    $("#content").change_to(errmsg(xhr)); 
-            } });
-
-    }
-
     my.back = function(){
-        my.current = (my.current > 0 ? my.current - 1 : my.current);
+        (my.current > 0) && (my.current -= 1);
         go(my.history[my.current]);
     };
 
     my.forward = function(){
-        my.current = (my.current <= my.history.length - 1 ? my.current + 1 : my.current);
+        (my.current <= my.history.length - 1) && (my.current += 1);
         go(my.history[my.current])
     };
 
@@ -40,4 +23,44 @@ var NavHistory = function(){
             my.current = my.history.length - 1;
         })
     }
+
+    // private
+    var go = function(url, success){
+
+        $.ajax({ url: url,
+
+            success: function(data, status, xhr){
+                change_to(data); 
+                if (success){
+                    success.call();
+                }
+            },
+
+            error: function(xhr){
+                change_to(errmsg(xhr)); 
+            } });
+
+    };
+
+    var change_to = function(data){
+        // Manage content transistion
+        my.elem.fadeOut(
+            0,
+            function(){
+                my.elem.html(data);
+                my.elem.fadeIn();
+            }
+        );
+    };
+
+    var errmsg = function(xhr){
+        switch (xhr.status){
+        case 404:
+            return $("<p><span class='error'>That file does not seem to exist</span>");
+            break;
+        default:
+            return $("<p><span class='error'>There was an error handling that click.  My fault.  Yell at me about it at jcarter@str.com</span>")
+        }
+    }
+
 };
